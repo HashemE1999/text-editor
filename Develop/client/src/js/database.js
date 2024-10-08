b75635c3518e70489268907697a5b2b80517dc18
Halcyon <hashem_elmaghrabi@yahoo.com>
2024-10-08 14:43:59 -0400

    Copied starter code, added workbox plugins and loaders to webpack.config.js, asset caching to src-sw.js, and get and put routes to database.js

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,37 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async () => {
+  console.log('PUT to the database');
+  const noteDb = await openDB('note', 1);
+  const tx = noteDb.transaction('note', 'readwrite');
+  const store = tx.objectStore('note');
+  const request = store.put({ id: id, todo: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("GET all from the database");
+  const noteDb = await openDB("note", 1);
+  const tx = noteDb.transaction("note", "readonly");
+  const store = tx.objectStore("note");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
+
+initdb();
